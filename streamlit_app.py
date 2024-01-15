4c8c2f77d26622ee2f3430906b1ef120ee8084cc
santosm24 <79465065+santosm24@users.noreply.github.com>
2024-01-15 22:06:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,6 @@ streamlit.dataframe(fruityvice_normalized)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+
+my_cnx.excecute(f"Insert into fruit_load_list value({add_my_fruit})")
